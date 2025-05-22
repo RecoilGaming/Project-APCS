9c656b6c64b48955ba7d6b676903b14280c78408
RecoilGaming <qinzhaolee@gmail.com>
2025-05-22 16:40:12 -0700

    Update Immortal.java

--- ljaag/src/main/java/com/apcs/ljaag/nodes/characters/Immortal.java
+++ ljaag/src/main/java/com/apcs/ljaag/nodes/characters/Immortal.java
@@ -1,16 +1,37 @@
 package com.apcs.ljaag.nodes.characters;
 
+import com.apcs.disunity.game.nodes.FieldChild;
 import com.apcs.disunity.game.nodes.Node;
+import com.apcs.disunity.game.nodes.SelectorNode;
 import com.apcs.disunity.game.nodes.collider.Collider;
+import com.apcs.disunity.game.nodes.sprite.AnimatedSprite;
+import com.apcs.disunity.game.nodes.sprite.Sprite;
 import com.apcs.disunity.game.nodes.twodim.Body;
 import com.apcs.disunity.game.physics.CollisionInfo;
 import com.apcs.disunity.math.Transform;
 import com.apcs.disunity.math.Vector2;
+import com.apcs.ljaag.nodes.indexed.InputVector;
 import com.apcs.ljaag.nodes.stats.Statset;
 
 public class Immortal extends Body {
 
+	/* ================ [ NODES ] ================ */
+
+	@FieldChild
+    private final SelectorNode<String, AnimatedSprite> sprite;
+
 	/* ================ [ FIELDS ] ================ */
+
+	{
+        AnimatedSprite s1, s2;
+        sprite = new SelectorNode<String, AnimatedSprite>(
+            s1 = new AnimatedSprite("idle", "player/player.png", Double.MAX_VALUE),
+            s2 = new AnimatedSprite("run", "player/run.png", 0.15, 0.15, 0.15, 0.15, 0.15, 0.15)
+		);
+
+        s1.setRotationType(Sprite.RotationType.BIDIRECTIONAL);
+        s2.setRotationType(Sprite.RotationType.BIDIRECTIONAL);
+    }
 
 	// Default values
 	protected final Statset STATS = new Statset();
@@ -21,6 +42,9 @@ public class Immortal extends Body {
 	// Resource amounts
 	private int health = 100;
 	private int aura = 100;
+
+	// Movement input
+	private final InputVector moveDir = new InputVector("move");
 
 	// Constructors
 	public Immortal(Transform transform, Node<?>... children) {
