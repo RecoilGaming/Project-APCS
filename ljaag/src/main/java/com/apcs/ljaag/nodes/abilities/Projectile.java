184ba92f1a0f094972e0c64e70eb4745e415197c
RecoilGaming <qinzhaolee@gmail.com>
2025-05-28 11:24:29 -0700

    Update ability with source uuid

--- ljaag/src/main/java/com/apcs/ljaag/nodes/abilities/Projectile.java
+++ ljaag/src/main/java/com/apcs/ljaag/nodes/abilities/Projectile.java
@@ -1,29 +1,30 @@
 package com.apcs.ljaag.nodes.abilities;
 
+import java.util.UUID;
 import java.util.function.BiConsumer;
 
+import com.apcs.disunity.app.interfaces.TriConsumer;
 import com.apcs.disunity.game.nodes.collider.Collider;
 import com.apcs.disunity.game.nodes.sprite.Sprite;
 import com.apcs.disunity.game.nodes.twodim.Area2D;
 import com.apcs.disunity.game.nodes.twodim.Body;
 import com.apcs.disunity.game.physics.BodyEntered;
 import com.apcs.disunity.math.Transform;
-import com.apcs.disunity.math.Vector2;
 
 public class Projectile extends Body {
 
 	/* ================ [ FIELDS ] ================ */
 
-	// Aim direction
-	private final Vector2 direction;
+	// Source UUID
+	private final UUID source;
 
 	// Update function
-	private final BiConsumer<Transform, Double> update;
+	private final TriConsumer<UUID, Transform, Double> update;
 
 	// Constructors
-	public Projectile(Collider collider, Area2D area2D, Sprite sprite, Vector2 direction, BiConsumer<Transform, Double> update) {
-		super(collider, area2D, sprite);
-		this.direction = direction;
+	public Projectile(UUID source, Transform transform, Collider collider, Area2D area2D, Sprite sprite, TriConsumer<UUID, Transform, Double> update) {
+		super(transform, collider, area2D, sprite);
+		this.source = source;
 		this.update = update;
 	}
 
@@ -39,7 +40,7 @@ public class Projectile extends Body {
 
 	@Override
 	public void update(Transform offset, double delta) {
-		update.accept(offset, delta);
+		update.accept(source, offset, delta);
 		super.update(offset, delta);
 	}
 	
