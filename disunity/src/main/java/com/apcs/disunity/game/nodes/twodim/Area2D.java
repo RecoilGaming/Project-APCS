2fb4a95a2b3d9cac74202aab267f4781a1386888
hem1sphereCHS <ttakeuchi141@student.fuhsd.org>
2025-06-02 12:20:43 -0700

    make collision layer an enum

--- disunity/src/main/java/com/apcs/disunity/game/nodes/twodim/Area2D.java
+++ disunity/src/main/java/com/apcs/disunity/game/nodes/twodim/Area2D.java
@@ -9,9 +9,11 @@ import com.apcs.disunity.game.signals.Signal;
 import com.apcs.disunity.math.Transform;
 import com.apcs.disunity.math.Vector2;
 
+/// node that detects if collider is colliding with itself.
 public class Area2D extends Node2D<Node<?>> {
 
     public final Signal<BodyEntered> bodyEnteredSignal = new Signal<>(BodyEntered.class);
+    /// determines which layers can collide with this area
     public final CollisionMask MASK;
     public Vector2 size;
 
--- disunity/src/main/java/com/apcs/disunity/game/physics/CollisionLayer.java
+++ disunity/src/main/java/com/apcs/disunity/game/physics/CollisionLayer.java
@@ -1,14 +1,22 @@
 package com.apcs.disunity.game.physics;
 
-/// represents which layer the collider lives in, in a form of bitset
-public class CollisionLayer {
-    public final int LAYER_ID;
+/// Represents collision layers available in game.
+/// nth bit of BITSET is set to 1, where n is the ordinal.
+/// You can "name" layers by storing them in a static final field.
+/// static import is recommended upon using.
+public enum CollisionLayer {
+    LAYER_0,  LAYER_1,  LAYER_2,  LAYER_3,  LAYER_4,  LAYER_5,  LAYER_6,  LAYER_7,
+    LAYER_8,  LAYER_9,  LAYER_10, LAYER_11, LAYER_12, LAYER_13, LAYER_14, LAYER_15,
+    LAYER_16, LAYER_17, LAYER_18, LAYER_19, LAYER_20, LAYER_21, LAYER_22, LAYER_23,
+    LAYER_24, LAYER_25, LAYER_26, LAYER_27, LAYER_28, LAYER_29, LAYER_30, LAYER_31;
     public final int BITSET;
 
-    public CollisionLayer(int layerId) {
+    CollisionLayer() {
+        BITSET = 1<<this.ordinal();
+    }
+    public static CollisionLayer of(int layerId) {
         if (layerId >= Integer.SIZE || layerId < 0)
             throw new IllegalArgumentException("layerId was out of supported range");
-        this.LAYER_ID = layerId;
-        BITSET = 1 << layerId;
+        return values()[layerId];
     }
 }
